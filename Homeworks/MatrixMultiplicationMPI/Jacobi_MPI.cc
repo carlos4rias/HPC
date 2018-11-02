f3463903858b673a6f9fbb9bb3bbd7631890ea22
Carlos Arias <carlos.23325@gmail.com>
2018-11-02 14:42:09 -0500

    test MPI

---
+++ Homeworks/MatrixMultiplicationMPI/Jacobi_MPI.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+#include "timer.hh"
+
+using namespace std;
+
+void jacobi(int nsweeps, int n, vector<double> &u, vector<double> &f) {
+  double h = 1.0 / n;
+  double h2 = h * h;
+  vector<double> utmp(n + 1);
+  utmp[0] = u[0];
+  utmp[n] = u[n];
+
+  for ( int sweep = 0; sweep < nsweeps; sweep += 2 ) {
+    for ( int i = 1; i < n; ++i )
+      utmp[i] = (u[i - 1] + u[i + 1] + h2 * f[i]) / 2;
+
+    for ( int i = 1; i < n; ++i )
+      u[i] = (utmp[i - 1] + utmp[i + 1] + h2 * f[i]) / 2;
+  }
+}
+
+void write_solution(int n, vector<double> &u, const char* fname) {
+  double h = 1.0 / n;
+  ofstream output;
+
+  output.open (fname);
+  for ( int i = 0; i <= n; ++i )
+    output << i * h << " " << u[i] << endl;
+  output.close();
+}
+
+int main (int argc, char** argv) {
+  int n = (argc > 1) ? atoi(argv[1]) : 100;
+  int nsteps = (argc > 2) ? atoi(argv[2]) : 100;
+  char* fname = (argc > 3) ? argv[3] : NULL;
+  double h = 1.0 / n;
+
+  vector<double> u(n + 1), f(n + 1);
+  for ( int i = 0; i <= n; ++i )
+    f[i] = i * h;
+  
+  {
+    Timer tmt("\"Elapsed time:\"");
+    jacobi(nsteps, n, u, f);
+    cout << "{" << endl;
+    cout << "\"N\": " << n << "," << endl;
+    cout << "\"Steps\": " << nsteps << "," << endl;
+  }
+  cout << "}," << endl;
+
+  if ( fname ) write_solution(n, u, fname);
+
+  return 0;
+}
---
+++ Homeworks/MatrixMultiplicationMPI/test_MPI.cc
@@ -0,0 +1,30 @@
+#include <mpi.h>
+#include <stdio.h>
+
+
+int main(int argc, char** argv) {
+  // Initialize the MPI environment
+  MPI_Init(NULL, NULL);
+
+  int world_size;
+  int world_rank;
+
+  // Get the number of processes
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+  // Get the rank of the process
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+  // Get the name of the processor
+  char processor_name[MPI_MAX_PROCESSOR_NAME];
+  int name_len;
+  MPI_Get_processor_name(processor_name, &name_len);
+
+  // Print off a hello world message
+  printf(
+    "Hello world from processor %s, rank %d out of %d processors\n",
+    processor_name, world_rank, world_size
+  );
+
+  // Finalize the MPI environment.
+  MPI_Finalize();
+}
